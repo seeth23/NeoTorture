e82acc6950184367384f5b31f211c2219819081b
seeth23 <ilyadit182@gmail.com>
2022-01-22 02:06:37 +0300

    Added editing system

--- src/main.c
+++ src/main.c
@@ -9,7 +9,6 @@
 #include "key_handle.h"
 #include "window_information.h"
 
-
 void init();
 void close();
 
@@ -24,16 +23,40 @@ int main(int argc, char **argv)
 
     init();
     
-    //win_info winYX = get_wininfo();
+    win_info winYX = get_wininfo();
     
     char *file_content = load_file(file_path);
     
-    printw("%s", file_content);
+    //    printw("%s", file_content);
+    addstr(file_content);
     move(0, 0);
-    handle_input(0);
     
+    handle_input(0);
+
+    char buf[1024][1024];
+    //int n = mvinch(0, 0);
+    
+    getch();
     close();
     //printf("height: %d, width %d\n", winYX.y_height, winYX.x_width);
+    int i, j;
+
+    for (i = 0; i < 50; i++) {
+        for (j = 0; j < 50; j++) {
+            buf[i][j] = mvinch(i, j);
+        }
+    }
+
+    buf[49][49] = '\0';
+    
+    for (int i = 0; i < 50; i++) {
+        for (int j = 0; j < 50; j++) {
+            printf("%c", buf[i][j]);
+        }
+        printf("\n");
+    }
+    
+    free(file_content);
     return 0;
 }
 
@@ -43,6 +66,7 @@ void init()
     initscr();
     noecho();
     keypad(stdscr, TRUE);
+    cbreak();
 }
 
 void close()
