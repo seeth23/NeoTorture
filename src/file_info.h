445bfa0544970447ef12b2d50e91306662c2ab8a
seeth23 <ilyadit182@gmail.com>
2022-01-23 02:59:11 +0300

    new-line counter

--- src/file_info.h
+++ src/file_info.h
@@ -13,16 +13,5 @@ typedef struct {
 } file_information;
 
 file_information file_info_init(size_t initsize);
-/*file_information
-file_info_init(size_t initsize)
-{
-    file_information this;
-    memset(&this, 0, sizeof(file_information));
-    this.init_size = initsize;
-    this.new_size = 0;
-    this.change_size = 0;
-    this.changed = false;
-    return this;
-    }*/
-//void handle_input(int *buf_to_edit, file_information *info_ptr);
+
 #endif
--- src/main.c
+++ src/main.c
@@ -12,6 +12,11 @@
 #include "current_time.h"
 #include "file_info.h"
 
+typedef struct {
+    size_t column;
+    int32_t null_term;
+} new_line_struct;
+
 bool save_window(char *file_path);
 void init();
 void close();
@@ -33,9 +38,30 @@ main(int argc, char **argv)
 
     char *file_content = load_file(file_path);
 
+    // COUNT FOR '\n' CHARACTERS
+    size_t new_line_count = 0;
+    for (size_t i = 0; i < strlen(file_content); i++) {
+        if (file_content[i] == '\n') {
+            new_line_count++;
+        }
+    }
+    size_t line_structSIZE = sizeof(new_line_struct) * new_line_count;
+    new_line_struct *new_line_map = malloc(line_structSIZE);
+    memset(new_line_map, 0, line_structSIZE);
+    if (new_line_map == NULL) {
+        fprintf(stderr, "Failed to alloc memory\n");
+        exit(EXIT_FAILURE);
+    }
+    int j = 0;
+    for (size_t i = 0; i < strlen(file_content); i++) {
+        if (file_content[i] == '\n') {
+            new_line_map[j].column = i;
+            new_line_map[j++].null_term = '\n';
+        }
+    }
+
     // Initialize file_information struct
     file_information file_info = file_info_init(strlen(file_content));
-    //buffer_size = file_info.init_size;
 
     for (size_t i = 0; i < file_info.init_size; i++) {
         buf[i] = file_content[i];
@@ -46,11 +72,11 @@ main(int argc, char **argv)
     while (*ptr) {
         addch(*ptr++);
     }
+
     free(file_content);
     ptr = NULL;
 
     // MAIN FUNCTION
-
     handle_input(buf, &file_info);
     close();
 
@@ -63,8 +89,14 @@ main(int argc, char **argv)
         printf("Saved %d bytes\n", saved);
         free(final_buf);
     }
-
+    printf("\n");
+    printf("SIZEOF STRUCTURE: %ld\n", line_structSIZE);
+    for (size_t i = 0; i < new_line_count; i++) {
+        printf("%ld row has new-line on %ld col\n", i, new_line_map[i].column);
+    }
+    printf("Change between init_size and new_size: %ld\n", file_info.change_size);
     free(buf);
+    free(new_line_map);
     return 0;
 }
 
